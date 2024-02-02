202a6448deabf5ba8385e0889840f7b5c2a83a05
Carlos Perales <carlosperalesa@gmail.com>
2024-02-02 18:10:00 -0300

    primer commit

---
+++ assets/js/js.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    //Smooth Scroll
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 500, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //Tooltip boton Siguiente en carousel
+    $('#next').tooltip({
+        title: 'Siguiente imagen',
+        placement: 'auto'
+    });
+
+    //Tooltip boton Anterior en carousel
+    $('#prev').tooltip({
+        title: 'Imagen anterior',
+        placement: 'auto'
+    });
+
+    //Desaparece destacados
+    $('#desid').on('click', function () {
+        $('#dessec').toggle();
+    });
+
+    //Modal boton Enviar
+    $('#enviar').on('click', function(){
+        $('#miModal').modal('show');
+    });
+
+    //ocultar boton Aceptar
+    $('#aceptar').on('click', function(){
+        $('#miModal').modal('hide');
+    });
+    
+});
